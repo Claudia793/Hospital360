1f2699868449a544451f58aedb3408cb5c7f494e
Claudia Montero Mena <jannymon7@gmail.com>
2026-07-23 18:32:17 -0600

    feat(login): agregar modelo DAL de usuarios Creación de carpetas dentro de las capas Creación de frmHospital360 en PL_Hospital360

---
+++ PL_Hospital360/Hospital360/frmHospital360.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PL_Hospital360.Hospital360
+{
+    public partial class frmHospital360 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
